bc54942fc2387bacda8b6360beae3f626b1be21a
Phil Boyd <phil.boyd@csat.com>
2022-05-18 09:40:16 -0400

    wp(az-tf-assoc): in flight - branch change

--- az-tf-assoc/mod-20-remote-state/storage/key_vault_access_policy.storage.resource.tf
+++ az-tf-assoc/mod-20-remote-state/storage/key_vault_access_policy.storage.resource.tf
@@ -7,6 +7,7 @@ resource "azurerm_key_vault_access_policy" "storage" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_storage_account.remote_state.identity[0].principal_id
 
-  key_permissions    = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
-  secret_permissions = ["Get"]
+  key_permissions         = ["Get", "Create", "List", "Update", "Delete", "Backup", "Import", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
+  secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"]
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
 }
